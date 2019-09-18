2ad819cfdd027f372fb81ceaf7fd467285254731
Onno Eberhard <onnoeberhard@gmail.com>
2019-09-18 16:15:44 +0200

    more sage support

--- Contents/MacOS/clean.py
+++ Contents/MacOS/clean.py
@@ -2,5 +2,5 @@ import os, re
 
 os.chdir(r"/Users/onno/Projects/stuff/")
 for f in os.listdir():
-    if re.match(r"^\d{4}-\d\d-\d\d(-sage)?.ipynb$", f) and os.stat(f).st_size in (68, 555):
+    if re.match(r"^\d{4}-\d\d-\d\d(-sage)?.ipynb$", f) and os.stat(f).st_size in (68, 555, 550):
         os.remove(f)
